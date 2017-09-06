016eaa2dc0c1f112ca3b72a40216297e7b56e0b4
Yinka Adetunji <yinka.adetunji@hva.nl>
2017-09-06 16:14:08 +0200

    first commit enjoy dates.py

---
+++ dates.py
@@ -0,0 +1,27 @@
+li = list()
+months = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
+has31Days = True
+
+class Month(object):
+	def __init__(self, arg, arg2):
+		super(Month, self).__init__()
+		self.name = arg
+		self.numberOfDays = arg2
+	def __str__(self):
+		return self.name + " : dagen : " + str(self.numberOfDays)
+
+for x in range(0,12):
+	if has31Days:
+		has31Days = False
+		li.append(Month(months[x], 31))
+	elif months[x] == months[1]:
+		li.append(Month(months[x], 28))
+		has31Days = True
+	elif months[x] == months[7]:
+		li.append(Month(months[x], 31))
+	else:
+		li.append(Month(months[x], 30))
+		has31Days = True
+
+for x in li:
+	print(x)
